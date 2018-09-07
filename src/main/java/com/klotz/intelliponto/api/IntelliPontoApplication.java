74f4cbdb93e7ae70f790089ab82be38aff7d5497
D-klotz <danielfelipeklotz@gmail.com>
2018-09-07 09:21:54 -0300

    commit inicial dos arquivos do projeto

---
+++ src/main/java/com/klotz/intelliponto/api/IntelliPontoApplication.java
@@ -0,0 +1,12 @@
+package com.klotz.intelliponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliPontoApplication.class, args);
+	}
+}
