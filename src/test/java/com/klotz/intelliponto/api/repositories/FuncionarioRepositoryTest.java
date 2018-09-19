bd1c770af23ef36f84919889418eaa2ebed3a278
Daniel Klotz <danielfelipeklotz@gmail.com>
2018-09-19 06:12:02 -0300

    Adicionado teste unitario para funcionario

---
+++ src/test/java/com/klotz/intelliponto/api/repositories/FuncionarioRepositoryTest.java
@@ -0,0 +1,94 @@
+package com.klotz.intelliponto.api.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.klotz.intelliponto.api.entities.Empresa;
+import com.klotz.intelliponto.api.entities.Funcionario;
+import com.klotz.intelliponto.api.enums.PerfilEnum;
+import com.klotz.intelliponto.api.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class FuncionarioRepositoryTest {
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String EMAIL = "email@email.com";
+	private static final String CPF = "080182888993";
+	
+	@Before
+	public void setUp() throws Exception{
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+	}
+	
+	@After
+	public final void tearDown() {
+		this.empresaRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorEmail() {
+		Funcionario funcionario = this.funcionarioRepository.findByEmail(EMAIL);
+		assertEquals(EMAIL, funcionario.getEmail());
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorCpf() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpf(CPF);
+		assertEquals(CPF, funcionario.getCpf());
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorEmailCpf() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, EMAIL);
+		assertNotNull(funcionario);
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorEmailOuCpfParaEmailInvalido() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, "email@invalido.com");
+		assertNotNull(funcionario);
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorEmailOuCpfParaCpfInvalido() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail("49837493498", EMAIL);
+		assertNotNull(funcionario);
+	}
+	
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("John Snow");
+		funcionario.setCpf(CPF);
+		funcionario.setEmail(EMAIL);
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456789"));
+		funcionario.setEmpresa(empresa);
+		return funcionario;
+	}
+	
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Patrula da noite SA");
+		empresa.setCnpj("4364347364");
+		return empresa;
+	}
+	
+}
